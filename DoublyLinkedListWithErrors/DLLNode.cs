506d90c52b2f99dcb7194ef8ee54975eaf3d70fc
conradpaterson <cpaterson52@gmail.com>
2024-11-17 14:38:57 +1300

    Moved isPrime method out of DLLNode.

--- DoublyLinkedListWithErrors/DLLNode.cs
+++ DoublyLinkedListWithErrors/DLLNode.cs
@@ -6,21 +6,44 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedListWithErrors
 {
-   public class DLLNode
-   {
+    public class DLLNode
+    {
         public int num;   // field of the node
         public DLLNode next; // pointer to the next node
         public DLLNode previous; // pointer to the previous node
-        public DLLNode (int num)
+        public DLLNode(int num)
         {
             this.num = num;
             next = null;
             previous = null;
         }// end of constructor
 
-        public Boolean isPrime(int n)
+        //public Boolean isPrime(int n)
+        //{
+        //    Console.WriteLine("Checking if number is prime: " + n);
+        //    Boolean b = true;
+        //    if (n < 2)
+        //    {
+        //        return false;
+        //    }
+        //    for (int i = 2; i <= Math.Sqrt(n); i++)
+        //    {
+        //        if (n % i == 0)
+        //        {
+        //            b = false;
+        //            break;
+        //        }
+        //    }
+        //    return b;
+        //}
+
+
+    } // end of class DLLNode
+
+    public static class MathUtilities
+    {
+        public static Boolean isPrime(int n)
         {
-            Console.WriteLine("Checking if number is prime: " + n);
             Boolean b = true;
             if (n < 2)
             {
@@ -36,28 +59,6 @@ namespace DoublyLinkedListWithErrors
             }
             return b;
         }
-
-
-    } // end of class DLLNode
-
-    //public static class MathUtilities
-    //{
-    //    public static Boolean isPrime(int n)
-    //    {
-    //        Boolean b = true;
-    //        if (n < 2)
-    //        {
-    //            return false;
-    //        }
-    //        for (int i = 2; i <= Math.Sqrt(n); i++)
-    //        {
-    //            if (n % i == 0)
-    //            {
-    //                b = false;
-    //                break;
-    //            }
-    //        }
-    //        return b;
-    //    }
+    }
 }
 
